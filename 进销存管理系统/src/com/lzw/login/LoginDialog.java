8458484b0fa36a12e834facf83d245394a198266
dream417 <eiwyh2012@gmail.com>
2017-06-09 14:03:25 +0800

    创建进销存管理系统工程

---
+++ 进销存管理系统/src/com/lzw/login/LoginDialog.java
@@ -0,0 +1,14 @@
+package com.lzw.login;
+
+import javax.swing.JFrame;
+
+public class LoginDialog extends JFrame {
+
+	private static final long seriaVersionUID=1L;
+	private LoginPanel loginPanel = null;
+	private 
+	
+	public static void main(String []srgs){
+		
+	}
+}
---
+++ 进销存管理系统/src/com/lzw/login/LoginPanel.java
@@ -0,0 +1,19 @@
+package com.lzw.login;
+
+import java.awt.ImageCapabilities;
+import java.net.URL;
+
+import javax.swing.JPanel;
+
+public class LoginPanel extends JPanel {
+	public int width,height;
+
+	public LoginPanel() {
+		super();
+		URL url = getClass().getResource("/res/login.jpg");
+		img = 
+	}
+	private Image;
+	
+	
+}
