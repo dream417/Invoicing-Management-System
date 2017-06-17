e6bbd2850cda9a6784761678eff4dc8c86d3b1c1
dream417 <eiwyh2012@gmail.com>
2017-06-17 19:51:25 +0800

    完成MenuBar类中的Menu相关方法，menuItem方法暂未实现

--- 进销存管理系统/src/com/lzw/MenuBar.java
+++ 进销存管理系统/src/com/lzw/MenuBar.java
@@ -1,6 +1,10 @@
 package com.lzw;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.beans.PropertyVetoException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +15,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 public class MenuBar extends JMenuBar {
 
@@ -26,7 +32,7 @@ public class MenuBar extends JMenuBar {
 	
 	private JMenu xinxi_chaxunMenu = null;
 	
-	private JMenu jjiben_ziliaoMenu = null;
+	private JMenu jiben_ziliaoMenu = null;
 	
 	private JMenu xitong_weihuMenu = null;
 	
@@ -112,67 +118,154 @@ public class MenuBar extends JMenuBar {
 
 	public JMenu getBang_zhuMenu() {
 		// TODO Auto-generated method stub
+		if(bang_zhuMenu == null){
+			bang_zhuMenu = new JMenu();
+			bang_zhuMenu.setText("帮助(H)");
+			bang_zhuMenu.setMnemonic(KeyEvent.VK_H);
+			bang_zhuMenu.add(getGuanyu_Item());
+			bang_zhuMenu.add(getBugItem());
+			bang_zhuMenu.add(getFangwen_wangzhanItem());
+		}
+		return bang_zhuMenu;
+	}
+
+	public JMenuItem getGuanyu_Item() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenuItem getBugItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenuItem getFangwen_wangzhanItem() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public JMenu getChuang_kouMenu() {
 		// TODO Auto-generated method stub
+		if(chuang_kouMenu == null){
+			chuang_kouMenu = new JMenu();
+			chuang_kouMenu.setText("窗口(W)");
+			chuang_kouMenu.setMnemonic(KeyEvent.VK_W);
+			chuang_kouMenu.addMenuListener(new MenuListener() {
+				
+				@Override
+				public void menuSelected(MenuEvent arg0) {
+					// TODO Auto-generated method stub
+					chuang_kouMenu.removeAll();
+					chuang_kouMenu.add(getPingpuItem());
+					chuang_kouMenu.add(getClassAllItem());
+					chuang_kouMenu.add(getAllIconItem());
+					chuang_kouMenu.add(getAllResumeItem());
+					chuang_kouMenu.addSeparator();
+					
+					int count = 0;
+					JInternalFrame[] allFrames = desktopPanel.getAllFrames();
+					for(final JInternalFrame frame : allFrames){
+						String frameTitle = frame.getTitle();
+						count++;
+						final JMenuItem item = new JMenuItem(count + " " + frameTitle);
+						chuang_kouMenu.add(item);
+						item.addActionListener(new ActionListener() {
+							
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								// TODO Auto-generated method stub
+								try{
+									frame.setIcon(false);
+									frame.setSelected(true);
+								}catch (PropertyVetoException e1){
+									e1.printStackTrace();
+								}
+							}
+						});
+					}
+				}
+
+				@Override
+				public void menuDeselected(MenuEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void menuCanceled(MenuEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		}
+		return chuang_kouMenu;
+	}
+
+	public JMenuItem getPingpuItem() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
+
+	public JMenuItem getClassAllItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public JMenuItem getAllIconItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public JMenuItem getAllResumeItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	public JMenu getXitong_weihuMenu() {
+		// TODO Auto-generated method stub
+		if(xitong_weihuMenu == null){
+			xitong_weihuMenu = new JMenu();
+			xitong_weihuMenu.setText("系统维护(S)");
+			xitong_weihuMenu.setMnemonic(KeyEvent.VK_S);
+			xitong_weihuMenu.add(getSHuju_beifenItem());
+			xitong_weihuMenu.addSeparator();
+			xitong_weihuMenu.add(getMima_xiugaiItem());
+			xitong_weihuMenu.addSeparator();
+			xitong_weihuMenu.add(getExitItem());
+		}
+		return xitong_weihuMenu;
+	}
+	
+	public JMenuItem getSHuju_beifenItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenuItem getMima_xiugaiItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenuItem getExitItem() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public JMenu getJiben_ziliaoMenu() {
 		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenu getXinxi_chaxunMenu() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenu getKucun_Menu() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenu getXiaoshou_Menu() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenu getJinhuo_Menu() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenuItem getJinhuoItem() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenuItem getXiaoshou_danItem() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenuItem getKucun_pandianItem() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenuItem getJiage_tiaozhengItem() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public JMenuItem getShangpin_chaxunItem() {
-		// TODO Auto-generated method stub
-		return null;
+		if(jiben_ziliaoMenu == null){
+			jiben_ziliaoMenu = new JMenu();
+			jiben_ziliaoMenu.setText("基本资料(B)");
+			jiben_ziliaoMenu.setMnemonic(KeyEvent.VK_B);
+			jiben_ziliaoMenu.add(getShangpin_guanliItem());
+			jiben_ziliaoMenu.add(getKehu_guanliItem());
+			jiben_ziliaoMenu.add(getGys_guanliItem());
+			jiben_ziliaoMenu.addSeparator();
+			jiben_ziliaoMenu.add(getJsr_guanliItem());
+		}
+		return jiben_ziliaoMenu;
 	}
 
 	public JMenuItem getShangpin_guanliItem() {
@@ -189,8 +282,104 @@ public class MenuBar extends JMenuBar {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public JMenuItem getJsr_guanliItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-	public JMenuItem getExitItem() {
+	public JMenu getXinxi_chaxunMenu() {
+		// TODO Auto-generated method stub
+		if(xinxi_chaxunMenu ==null){
+			xinxi_chaxunMenu = new JMenu();
+			xinxi_chaxunMenu.setText("信息查询(C)");
+			xinxi_chaxunMenu.setMnemonic(KeyEvent.VK_C);
+			xinxi_chaxunMenu.add(getXiaoshou_chaxunItem());
+			xinxi_chaxunMenu.add(getShangpin_chaxunItem());
+			xinxi_chaxunMenu.addSeparator();
+			xinxi_chaxunMenu.add(getXiaoshou_paihangItem());
+		}
+		return null;
+	}
+
+	public JMenuItem getXiaoshou_chaxunItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public JMenuItem getShangpin_chaxunItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenuItem getXiaoshou_paihangItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenu getKucun_Menu() {
+		// TODO Auto-generated method stub
+		if(kucun_Menu == null){
+			kucun_Menu = new JMenu();
+			kucun_Menu.setText("库存管理(K)");
+			kucun_Menu.setMnemonic(KeyEvent.VK_K);
+			kucun_Menu.add(getKucun_pandianItem());
+			kucun_Menu.add(getJiage_tiaozhengItem());
+		}
+		return kucun_Menu;
+	}
+	
+	public JMenuItem getKucun_pandianItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenuItem getJiage_tiaozhengItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenu getXiaoshou_Menu() {
+		// TODO Auto-generated method stub
+		if(xiaoshou_Menu == null){
+			xiaoshou_Menu = new JMenu();
+			xiaoshou_Menu.setText("销售管理(X)");
+			xiaoshou_Menu.setMnemonic(KeyEvent.VK_X);
+			xiaoshou_Menu.add(getXiaoshou_danItem());
+			xiaoshou_Menu.add(getXiaoshou_tuihuoItem());
+		}
+		return xiaoshou_Menu;
+	}
+
+	public JMenuItem getXiaoshou_danItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public JMenuItem getXiaoshou_tuihuoItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenu getJinhuo_Menu() {
+		// TODO Auto-generated method stub
+		if(jinhuo_Menu == null){
+			jinhuo_Menu = new JMenu();
+			jinhuo_Menu.setText("进货管理(J)");
+			jinhuo_Menu.setMnemonic(KeyEvent.VK_J);
+			jinhuo_Menu.add(getJinhuoItem());
+			jinhuo_Menu.add(getJinhuo_tuihuoItem());
+		}
+		return jinhuo_Menu;
+	}
+
+
+	public JMenuItem getJinhuoItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JMenuItem getJinhuo_tuihuoItem() {
 		// TODO Auto-generated method stub
 		return null;
 	}
